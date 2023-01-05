dd98e047b4aa3d0add01955a4f8a2e2d9aea9a62
musthaq96 <mushtaq96smb@gmail.com>
2023-01-05 20:31:28 +0100

    feature: custom loader/animation on request->server

--- frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
+++ frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
@@ -7,12 +7,14 @@ import { useState } from 'react';
 import { setAvatar } from '../../../store/activateSlice';
 import {activate} from '../../../http';
 import {setAuth} from '../../../store/authSlice';
+import Loader from '../../../components/shared/Loader/Loader';
 
 
 const StepAvatar = ({onNext}) => {
     const {name, avatar} = useSelector((state)=>state.activate);
     const dispatch = useDispatch();
     const [image, setImage] = useState('/images/profilePic-sample.png');
+    const [loading, setLoading] = useState(false);
     function captureImage(e){
         const file = e.target.files[0];
         //convert to base 64 stream
@@ -27,6 +29,7 @@ const StepAvatar = ({onNext}) => {
         console.log(e)
     }
     async function submit(){
+        setLoading(true); //start the animation 
         //use try catch for server requests
         try{
             const {data} = await activate({name, avatar})      
@@ -37,9 +40,13 @@ const StepAvatar = ({onNext}) => {
             console.log(data);      
         }catch(error){
             console.log(error);
+        }finally{
+            setLoading(false);//runs after try or catch phase
         }
 
     }
+
+    if(loading) return <Loader message={"Activation in progress..."}/>;
     return( 
     <>
         <Card title={`Okay, ${name}`} icon="goggle-emoji">
