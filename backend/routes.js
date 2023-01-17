ff631aafd97c1f3564103cb0c7ebe7e7654011ce
mushtaq96 <mushtaq96smb@gmail.com>
2023-01-17 20:33:13 +0100

    feat: (BE) add model info to db

--- backend/routes.js
+++ backend/routes.js
@@ -1,17 +1,21 @@
-const router = require('express').Router();
-const authController = require('./controllers/auth-controller');
-const activateController = require('./controllers/activate-controller');
-const authMiddleware = require('./middlewares/auth-middleware');
+const router = require("express").Router();
+const authController = require("./controllers/auth-controller");
+const activateController = require("./controllers/activate-controller");
+const authMiddleware = require("./middlewares/auth-middleware");
+const roomsController = require("./controllers/rooms-controller");
 
-router.post('/api/send-otp', authController.sendOtp);
-router.post('/api/verify-otp', authController.verifyOtp);
+router.post("/api/send-otp", authController.sendOtp);
+router.post("/api/verify-otp", authController.verifyOtp);
 
-router.post('/api/activate', authMiddleware, activateController.activate);
+router.post("/api/activate", authMiddleware, activateController.activate);
 //this should be protected. allow this only for people who have valid access token
 
-router. get('/api/refresh', authController.refresh);
+router.get("/api/refresh", authController.refresh);
 
-router.post('/api/logout', authMiddleware, authController.logout);
+router.post("/api/logout", authMiddleware, authController.logout);
 //middle ward bcz we wanted only the loged in user to log out.
+
+router.post("/api/rooms", authMiddleware, roomsController.create);
+//middleware bcz we wanted only the loged in user to log out.
 
 module.exports = router;
